d76c8acee8e814179f75dd23ce797b910a1078ce
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2022-01-21 12:30:06 -0800

    Add files via upload

---
+++ Enterra Website/jsons/news.js
@@ -0,0 +1,12 @@
+var articles = [
+    {
+        id = 0,
+        title = "test Title",
+        htmlToAdd = "add html here"
+    },
+    {
+        id = 1,
+        title = "test Title 2",
+        htmlToAdd = "add html here"
+    }
+];
